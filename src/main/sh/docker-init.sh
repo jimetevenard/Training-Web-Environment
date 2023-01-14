51d1320a3ffff1c67e51e944a5e85bfdb00739a3
Jim ETEVENARD <jetevenard@oxiane.com>
2023-01-14 17:19:03 +0100

    Vscode : disable-workspace-trust

--- src/main/sh/docker-init.sh
+++ src/main/sh/docker-init.sh
@@ -7,4 +7,4 @@ sudo nginx
 # Lancement code-server
 # (process attaché, doit être le dernier step)
 echo "[JIM] Lancons code-server"
-code-server --host 0.0.0.0 . --auth none /home/coder/project
+code-server --host 0.0.0.0 . --auth none --disable-workspace-trust /home/coder/project
